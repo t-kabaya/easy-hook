6e3b40e7ce1dd14e2d24497956b0e8da5f6b0de4
kaba <taikikabaya@gmail.com>
2020-01-07 11:32:27 +0900

    feat: add react

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const easyHooks = inputs => {
+import { useState } from 'react'
+
+const easyHooks = preStates => {
   const hooks = {}
-  inputs.forEach(input => {
-    const [state, setState] = useState(input[1])
-    hooks[input[0]] = state
-    hooks['set' + capitalize(input[0])] = setState
+  preStates.forEach(preState => {
+    const [state, setState] = useState(preState[1])
+    hooks[preState[0]] = state
+    hooks['set' + capitalize(preState[0])] = setState
   })
   return hooks
 }
